37a70d0294c8462b11def065fa23502458ed8203
Jenny Rogers <jbtaylor339@gmail.com>
2022-10-17 21:33:03 -0400

    tried zero inflated model and linear mixed effect model for pumpkinseed

--- tidying_FishTraits.R
+++ tidying_FishTraits.R
@@ -1,17 +1,59 @@
-library(RODBC) 
+library(readxl) 
+library(tidyverse)
 
 
-db <- "C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/FishTraits.accdb"
-con2 <- odbcConnectAccess2007(db)
-
-#get a list of the table names
-sqlTables(con2, tableType = "TABLE")$TABLE_NAME
-
-#bring in 3 of the tables into R as dataframes
-gillnet_data <- sqlFetch(con2, "Copy Of Fish_Traits")
-seine_data <- sqlFetch(con2, "seine_data")
-water_chem <- sqlFetch(con2, "water_chemistry")
 
 
-st_layers(dsn = "C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/FishTraits.accdb")
-flowmet_endofcen <- st_read("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/FishTraits.accdb", layer = "Copy Of Fish_Traits")
+
+dat<- read_excel("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/FishTraits/Copy Of Fish_Traits.xlsx")
+dat$COMMONNAME <- tolower(dat$COMMONNAME)
+dat$COMMONNAME[dat$COMMONNAME == "trout-perch"] <- "trout perch"
+dat$COMMONNAME[dat$COMMONNAME == "pearl dace"] <- "allegheny pearl dace"
+
+
+#read in fish occurrence data just to get the nameing conventions
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_occurrence.RData")
+names <- fish_occurrence %>% 
+  select(common_name, scientific_name) %>% 
+  unique()
+
+
+# test <- dat %>% 
+#   select(COMMONNAME, GENUS, SPECIES) %>% 
+#   rename(common_name = COMMONNAME)
+# 
+# test3 <- left_join(test, names, by = "common_name")
+# 
+# incorrect <- names %>% 
+#   filter(!common_name %in% test3$common_name)
+# 
+
+
+traits <- dat %>% 
+  select(FID, COMMONNAME, 29:115) %>% 
+  filter(COMMONNAME %in% names$common_name)
+
+spawntime <- traits %>% 
+  select(COMMONNAME, 7:18)
+
+spawnsub <- traits %>% 
+  select(COMMONNAME, 21:45)
+
+sub <- traits %>% 
+  select(COMMONNAME, 51:60)
+  
+vel <- traits %>% 
+  select(COMMONNAME, 74:76)
+
+habtype <- traits %>% 
+  select(COMMONNAME, 62:73)
+
+
+#left join with the traits assigned in New England
+traits <- read_xlsx("C:/Users/jrogers/Documents/necascFreshwaterBio/spp_data/ThermalPreferences.xlsx")
+traits <- traits %>% 
+  select(common_name, `Temperatures Notes`) %>% 
+  rename(tmp = `Temperatures Notes`)
+
+table(traits$tmp)
+
