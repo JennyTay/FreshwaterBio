150cd4f87b910d2f5c97ceb59dd862b94de00acf
Jenny Rogers <jbtaylor339@gmail.com>
2022-10-25 15:33:49 -0400

    R script for the fish model covariate tidying, progress on fish trait model

--- tidying_FishTraits.R
+++ tidying_FishTraits.R
@@ -4,11 +4,12 @@ library(tidyverse)
 
 
 
-
+#read in the fish traits database and fix some of the names
 dat<- read_excel("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/FishTraits/Copy Of Fish_Traits.xlsx")
-dat$COMMONNAME <- tolower(dat$COMMONNAME)
-dat$COMMONNAME[dat$COMMONNAME == "trout-perch"] <- "trout perch"
-dat$COMMONNAME[dat$COMMONNAME == "pearl dace"] <- "allegheny pearl dace"
+names(dat)[11] <- "common_name"
+dat$common_name <- tolower(dat$common_name)
+dat$common_name[dat$common_name == "trout-perch"] <- "trout perch"
+dat$common_name[dat$common_name == "pearl dace"] <- "allegheny pearl dace"
 
 
 #read in fish occurrence data just to get the nameing conventions
@@ -18,23 +19,23 @@ names <- fish_occurrence %>%
   unique()
 
 
-# test <- dat %>% 
-#   select(COMMONNAME, GENUS, SPECIES) %>% 
-#   rename(common_name = COMMONNAME)
+# test <- dat %>%
+#   select(common_name, GENUS, SPECIES) %>%
+#   rename(common_name = common_name)
 # 
 # test3 <- left_join(test, names, by = "common_name")
 # 
-# incorrect <- names %>% 
+# incorrect <- names %>%
 #   filter(!common_name %in% test3$common_name)
-# 
+
 
 #remove spp identifies that we don't need, and remove all the food items, which likely isnt applicable ot our study
 traits <- dat %>% 
-  select(FID, COMMONNAME, 29:115) %>% 
-  filter(COMMONNAME %in% names$common_name)
+  select(FID, common_name, 29:115) %>% 
+  filter(common_name %in% names$common_name)
 
 spawntime <- traits %>% 
-  select(COMMONNAME, 7:18) %>% 
+  select(common_name, 7:18) %>% 
   pivot_longer(cols = 2:13, names_to = "month", values_to = "value") %>% 
   mutate(season = ifelse(month %in% c("MAR", "APR", "MAY"), "spring",
                          ifelse(month %in% c("JUN", "JUL", "AUG"), "summer",
@@ -56,7 +57,7 @@ spawntime$month[spawntime$month == "DEC"] <- 12
 ggplot(data = spawntime, mapping = aes(x = as.integer(month), y = value))+
   geom_point()+
   geom_line()+
-  facet_wrap(~COMMONNAME)+
+  facet_wrap(~common_name)+
   scale_x_continuous(breaks = seq(1,12, by = 1))
 ggsave(
   filename = "C:/Users/jenrogers/Documents/git/FreshwaterBio/FreshwaterBio/tmpfigures/spawntime.png",
@@ -68,10 +69,10 @@ ggsave(
 )
 
 test <- spawntime %>% 
-  group_by(COMMONNAME, season) %>% 
+  group_by(common_name, season) %>% 
   summarise(tot = sum(value)) %>% 
   filter(tot != 0) %>% 
-  group_by(COMMONNAME) %>% 
+  group_by(common_name) %>% 
   pivot_wider(names_from = season, values_from = tot) %>% 
   mutate(timing = ifelse(spring > 0 & is.na(summer) & is.na(winter) & is.na(fall), "sp", 
                          ifelse(fall > 0 & is.na(summer) & is.na(winter) & is.na(spring),"f", 
@@ -83,16 +84,16 @@ test <- spawntime %>%
                                               "su"))))))))
                                        
 #for some reason the ifelse statement is not completing with the 'else' so ill add those manuall
-test$timing[test$COMMONNAME == "blacknose shiner"] <- "su"
-test$timing[test$COMMONNAME == "longnose sucker"] <- "su"
+test$timing[test$common_name == "blacknose shiner"] <- "su"
+test$timing[test$common_name == "longnose sucker"] <- "su"
 
     
 
 
 spawntime <- traits %>% 
-  select(COMMONNAME, 7:19) %>% 
+  select(common_name, 7:19) %>% 
   mutate(strategy = ifelse(SEASON > 2, "extended", "narrow")) %>% 
-  left_join(test, by = "COMMONNAME") %>% 
+  left_join(test, by = "common_name") %>% 
   select(1:15, 20) 
 
 write.csv(spawntime, "C:/Users/jenrogers/Documents/git/FreshwaterBio/FreshwaterBio/tmpfigures/spawntime.csv")
@@ -101,16 +102,27 @@ write.csv(spawntime, "C:/Users/jenrogers/Documents/git/FreshwaterBio/FreshwaterB
   
 
 spawnsub <- traits %>% 
-  select(COMMONNAME, 21:45)
+  select(common_name, 21:45)
 
 sub <- traits %>% 
-  select(COMMONNAME, 51:60)
+  select(common_name, 51:60)
   
 vel <- traits %>% 
-  select(COMMONNAME, 74:76)
+  select(common_name, 74:76)
+
+
+vel <- vel %>% 
+  mutate(velocity = ifelse(SLOWCURR == 1 & MODCURR == 0 & FASTCURR == 0, "slow",
+                           ifelse(MODCURR == 1 & SLOWCURR == 0 & FASTCURR == 0, "mod",
+                                  ifelse(FASTCURR == 1 & SLOWCURR == 0 & MODCURR == 0, "fast", 
+                                         ifelse(SLOWCURR == 1 & MODCURR == 1 & FASTCURR == 0, "slow-mod",
+                                                ifelse(SLOWCURR == 0 & MODCURR == 1 & FASTCURR == 1, "fast-mod", "any"))))))
+write.csv(vel, "C:/Users/jenrogers/Documents/git/FreshwaterBio/FreshwaterBio/tmpfigures/velocity.csv")
+
 
 habtype <- traits %>% 
-  select(COMMONNAME, 62:73)
+  select(common_name, 62:73)
+
 
 
 #left join with the traits assigned in New England
@@ -120,4 +132,20 @@ temp <- temp %>%
   rename(tmp = `Temperatures Notes`)
 
 table(temp$tmp)
-#use the traits database to fill in the ones we dont have
+
+
+
+
+#merge traits into a single dataframe
+names(spawntime)
+names(vel)
+names(temp)
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_count_with_zeros.RData")
+
+final_traits <- left_join(temp, spawntime, by = "common_name") %>% 
+  left_join(vel, by = "common_name") %>% 
+  select(common_name, tmp, strategy, timing, velocity) %>% 
+  filter(common_name %in% fish_count_with_zeros$common_name)
+
+
+save(final_traits, file = "C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_traits.RData")
---
+++ tidying_covariates.R
@@ -0,0 +1,239 @@
+
+
+
+#fish data
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_occurrence.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_count_with_zeros.RData")
+fish_shp <- st_read("C:/Users/jenrogers/Documents/necascFreshwaterBio/SpatialData/sppdata/all_fish_event.shp")
+
+#fish event and hydrography join
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_event_huc_join.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_event_flowline_join.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/fish_event_flowlineV2_join.RData")
+
+#USGS gauge data
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/flowmetrics/survey_huc8flowmetrics.RData")
+
+#SHEDs temp data
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/sheds_temp_metrics_huc12.RData")
+load(file = "C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/sheds_temp_metrics_annaul.RData")
+
+#watershed shape files
+huc8 <- st_read("C:/Users/jenrogers/Documents/necascFreshwaterBio/SpatialData/NHDplus/WBDHU8/WBDHU8_NE.shp")
+
+
+#USFS flow metrics
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/SpatialData/USFS flow metrics/flowmet_historical_crop.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/SpatialData/USFS flow metrics/flowmet_endofcen_crop.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/SpatialData/USFS flow metrics/flowmet_midcen_crop.RData")
+
+
+#StreamCat
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/strmcatByyr_census.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/strmcatByyr_landcov.RData") 
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/strmcatByyr_imp.RData")
+load("C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/strmcat_byCOMID.RData")
+
+
+
+
+
+
+
+event <- fish_event_huc_join %>% 
+  data.frame() %>% 
+  select(UID, state, year, month, huc8_name, huc12_name, huc8_areasqkm, huc12_areasqkm)
+
+flow <- sites %>% 
+  select(huc8_name, year, month, 14:22)
+
+
+#join huc8 flow to event data
+dat <- left_join(event, flow, by = c("huc8_name", "year", "month"))
+
+
+
+#join the temperature data at the huc12 scale by time period
+dat <- dat %>% 
+  mutate(timeperiod = ifelse(year <= 1999, "pre", "post")) %>% 
+  left_join(metrics12, by = c("huc12_name", "timeperiod"))
+
+#join the temperature data at the huc12 scale by year
+names(metrics_annual)[3:7] <- paste("annual", names(metrics_annual)[3:7], sep = "_")
+dat <- left_join(dat, metrics_annual, by = c("huc12_name", "year"))
+
+
+
+#join the flowlineV2 with the COMID
+fish_event_flowlineV2_join <- fish_event_flowlineV2_join %>% 
+  mutate(long = unlist(map(fish_event_flowlineV2_join$geometry, 1)),
+         lat = unlist(map(fish_event_flowlineV2_join$geometry, 2))) %>% 
+  data.frame() %>% 
+  select(-geometry)
+
+dat <- dat %>% 
+  select(-c(state, year, month)) %>% 
+  left_join(fish_event_flowlineV2_join, by = "UID")
+
+
+
+#join to the USFS flow metrics
+flowmet_historical_crop <- flowmet_historical_crop %>% 
+  data.frame() %>% 
+  select(COMID, TOTDASQKM, WBAREATYPE, 9:34) %>% 
+  mutate(COMID = as.integer(COMID))
+dat <- dat %>% 
+  left_join(flowmet_historical_crop, by = "COMID")
+
+
+#join to the streamcat data
+strmcatByyr_census <- strmcatByyr_census %>% 
+  select(-year)
+strmcatByyr_landcov <- strmcatByyr_landcov %>% 
+  select(-year)
+
+dat <- dat %>% 
+  left_join(strmcat_byCOMID, by = "COMID")
+dat <- dat %>% 
+  left_join(strmcatByyr_census, by = "COMID")#this is the 2010 data... we might want to do this year in the future..
+dat <- dat %>% 
+  left_join(strmcatByyr_landcov, by = "COMID")#this is 2001 NLCD data.. we probably will want to download a coupel more, but for now, I just did one year. they also have some that just look at the riparian buffer..
+dat <- dat %>% 
+  left_join(strmcatByyr_imp, by = c("COMID", "timeperiod"))
+
+
+#we probably want to add in data for road crossings and riparian area 
+
+#organize dataframe
+dat <- dat %>% 
+  select("UID", "COMID", "lat", "long",
+         "GNIS_ID", "GNIS_NAME", "state", 
+         "date", "year", "month", "timeperiod",   
+         "waterbody", "WBAREATYPE", "project", "source", "event_to_flowln_dist_m","REACHCODE", "huc8_name", "huc12_name", 
+         "LENGTHKM", "huc8_areasqkm",  "huc12_areasqkm", "WsAreaSqKm", 
+         "q_mean_rel", "q_max_rel" ,"low_dur_days", "high_dur_days", "min_month", "monthly_min_flow", 
+         "max_month", "monthly_max_flow", "max_temp", "mean_jul_temp", "mean_summer_temp", 
+         "mean_max_temp_30d", "mean_n_day_gt_22",  "annual_max_temp", "annual_mean_jul_temp", 
+         "annual_mean_summer_temp", "annual_mean_max_temp_30d", "annual_mean_n_day_gt_22", 
+         "MA_HIST", "MJAN_HIST",  "MFEB_HIST", "MMAR_HIST", "MAPR_HIST", "MMAY_HIST", 
+         "MJUN_HIST", "MJUL_HIST", "MAUG_HIST", "MSEP_HIST", "MOCT_HIST", "MNOV_HIST", "MDEC_HIST", "MDJF_HIST", 
+         "MMAM_HIST" , "MJJA_HIST" , "MSON_HIST", "HIQ1_5_HIST", "HIQ10_HIST", "HIQ25_HIST", "LO7Q1_HIST", 
+         "LO7Q10_HIST" ,"BFI_HIST", "LO7Q1DT_HIST", "CFM_HIST", "W95_HIST",
+         "BFIWs", "DamDensWs", "DamNIDStorWs", "DamNrmStorWs", "ElevWs", "NABD_DensWs", 
+         "NABD_NIDStorWs", "NABD_NrmStorWs", "WtDepWs", "HUDen_Ws",
+         "PopDen_Ws", "PctOw_Ws", "PctIce_Ws","PctUrbOp_Ws", "PctUrbLo_Ws", "PctUrbMd_Ws", "PctUrbHi_Ws",
+         "PctBl_Ws", "PctDecid_Ws", "PctConif_Ws", "PctMxFst_Ws", "PctShrb_Ws", "PctGrs_Ws", "PctHay_Ws", 
+         "PctCrop_Ws", "PctWdWet_Ws", "PctHbWet_Ws", "PctImp_Ws", "PctImp_WsRp100")
+
+#we did not include 'TOTDASQKM' from the USGS flow metrics because it was identical and sourced from 'WsAreaSqKm' in StreamCat
+
+
+
+
+
+
+
+
+colSums(is.na(dat))
+
+
+str(dat)
+
+#variance across study area
+
+nzv <- nearZeroVar(dat[,23:96], saveMetrics = T)
+
+nzv$metric <- row.names(nzv)
+remove <- nzv$metric[nzv$zeroVar == TRUE | nzv$nzv == TRUE]
+
+
+dat2 <- dat %>% 
+  select(-remove)
+
+#look for correlation between variables from the same dataset (SHEDs, huc8 streamflow, USGS flow metrics, Streamcat, metadata (lat,long, watershed size, elev, length) )
+
+
+usgsgague <- dat2 %>% 
+  select(24:31) 
+usgsgague <- usgsgague[complete.cases(usgsgague),] #remove rows with NA
+cor <- cor(usgsgague, method = c("spearman")) #make correlation matrix
+corrplot(cor, type = "lower") #plot
+#removes correlatioms 0.5 and higher
+keep1 <- c("low_dur_days", "high_dur_days", "min_month", "monthly_min_flow", "max_month") 
+#removes "q_mean_rel", "q_max_rel", "monthly_max_flow
+
+sheds <- dat2 %>% 
+  select(32:40)
+sheds <- sheds[complete.cases(sheds),]
+cor <- cor(sheds, method = c("spearman"))
+corrplot(cor, type = "lower")
+#these are all incredible correlated so we will just keep one - annual_mean_max_temp_30d
+
+keep2 <- "annual_mean_max_temp_30d" 
+#this removes "max_temp", "mean_jul_temp", "mean_summer_temp", "mean_max_temp_30d", "annual_max_temp",
+#              "annual_mean_jul_temp", "annual_mean_summer_temp", "annual_mean_n_day_gt_22"
+
+
+usfs <- dat2 %>% 
+  select(41:66)
+usfs <- usfs[complete.cases(usfs),] #remove rows with NA
+cor <- cor(usfs, method = c("spearman")) #make correlation matrix
+corrplot(cor, type = "lower") #plot
+#all of the magnitude variables are very correlated so we will just keep one, Mean JJA, and then we will keep the other non-magnitude variables, which were not correlated at all to anything esle
+
+keep3 <- c("MJJA_HIST","BFI_HIST", "LO7Q1DT_HIST", "CFM_HIST", "W95_HIST") 
+#this removes "MA_HIST", "MJAN_HIST","MFEB_HIST", "MMAR_HIST", "MAPR_HIST", "MMAY_HIST", "MJUN_HIST", 
+#             "MJUL_HIST","MAUG_HIST", "MSEP_HIST", "MOCT_HIST", "MNOV_HIST", "MDEC_HIST", "MDJF_HIST", 
+#             "MMAM_HIST", "MSON_HIST", "HIQ1_5_HIST", "HIQ10_HIST", "HIQ25_HIST", 
+#             "LO7Q1_HIST", "LO7Q10_HIST"
+
+strmct <- dat2 %>% 
+  select(67:87)
+strmct <- strmct[complete.cases(strmct),] #remove rows with NA
+cor <- cor(strmct, method = c("spearman")) #make correlation matrix
+corrplot(cor, type = "lower") #plot
+#the high, med, low, and impervious surface, and housing density, and population are all correlated, but not very very high... we migth want to do a pca with these, though that will make it harder to interpret.
+keep4 <-  c("BFIWs", "ElevWs", "WtDepWs", "PctOw_Ws", "PctUrbOp_Ws",
+            "PctBl_Ws", "PctDecid_Ws", "PctConif_Ws", "PctMxFst_Ws", "PctShrb_Ws", "PctGrs_Ws",
+            "PctHay_Ws", "PctWdWet_Ws", "PctHbWet_Ws", "PctImp_Ws", "PctImp_WsRp100")
+#removes: "PctUrbLo_Ws", "PctUrbMd_Ws", "PctUrbHi_Ws", "PopDen_Ws", "HUDen_Ws" --> all were correlated with pctimpervious
+
+
+dat3 <- dat2 %>% 
+  select(UID, lat, long, state, year, month, source, huc8_name,WsAreaSqKm, all_of(keep1), all_of(keep2), all_of(keep3), all_of(keep4)) %>% 
+  mutate(pctforest_Ws = PctDecid_Ws + PctConif_Ws + PctMxFst_Ws) %>%  #make a new variable for percent forest in genearl
+  select(-PctDecid_Ws, -PctConif_Ws, -PctMxFst_Ws) #remove the three specific types of forest
+
+#now check for correlation among the retained predictors
+final <- dat3 %>% 
+  select(2,3, 9:34)
+final <- final[complete.cases(final),] #remove rows with NA
+cor <- cor(final, method = c("spearman")) #make correlation matrix
+corrplot(cor, type = "lower") #plot
+
+
+
+
+#first make histograms of covariates to see whats normally distributed
+dat4 <- dat3 %>% 
+  select(2,3, 9:34)
+
+for (i in 1:28) {
+  
+  ggplot(data = dat4, mapping = aes(x = dat4[[i]]))+
+    geom_histogram(binwidth = )+
+    labs(x = names(dat4)[i])
+  
+  ggsave(filename = paste("C:/Users/jenrogers/Documents/git/FreshwaterBio/FreshwaterBio/tmpfigures/covariate_histograms/", names(dat4)[i], ".png", sep = ""),
+         plot = last_plot())
+  
+}
+
+
+#log transform certain variables
+dat3 <- dat3 %>% 
+  mutate(logWsAreaSqKm = log(WsAreaSqKm),
+         logMJJA_HIST = log(MJJA_HIST)) %>% 
+  select(-WsAreaSqKm, -MJJA_HIST )
+
+save(dat3, file = "C:/Users/jenrogers/Documents/necascFreshwaterBio/model_datafiles/model_covariates.RData")
